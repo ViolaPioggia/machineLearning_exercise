2af8249aa5b94de133027586da99946a1b170af9
violapioggia <2604296771@qq.com>
2024-01-10 13:47:56 +0800

    lesson03 update

--- lesson03/client_info.py
+++ lesson03/client_info.py
@@ -14,256 +14,112 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(data.iloc[:, :5], data.iloc
 [:, 5], test_size=0.3, shuffle=True)
 
-
 class Decision_Tree_C45:
-    # define entropy calculation
     def __init__(self):
-        self.tmp = 0
+        self.stump = [0, 0, 0, 0]  # feature_index, value, left_class, right_class
 
-    def Entropy(self, train_data):
-        """
-        Parameters
-        ----------
-        train_data : list
-        DESCRIPTION: list of training instances
-        Returns
-        -------
-        ent: entropy of data
-        """
-        inst_num = len(train_data)  # instances number
-        label_counts = {}  # count instances of each class
-        for i in range(inst_num):
-            label = train_data[i][-1]  # get instance class
-            if label not in label_counts.keys():
-                label_counts[label] = 0
-            label_counts[label] += 1  # count
-        ent = 0
-        for key in label_counts.keys():
-            # calculate each class proportion
-            prob = float(label_counts[key]) / inst_num
-            ent -= prob * log(prob, 2)  # see Eq.(3.1)
-        return ent
+    # 计算香农熵
+    def cal_Ent(self, dataset, weight):
+        sum = np.sum(weight)
+        lable_counts = {}   # 记录每个类的数量
+        for i in range(len(dataset)):
+            sample_class = dataset[i][-1]
+            if sample_class not in list(lable_counts.keys()):
+                lable_counts[sample_class] = 0
+            lable_counts[sample_class] += weight[i][0]  # 某类的某个样本的权重
 
-    # split data according to feature and feature value
-    def split_data(self, train_data, feature_index, feature_value, feature_type):
-        """
-        Parameters
-        ----------
-        train_data : list
-        DESCRIPTION: list of training instances
-        feature_index : int
-        DESCRIPTION: index of feature to split
-        feature_value : float,int or str
-        DESCRIPTION: feature value
-        feature_type : "D",L" or "R"
-        DESCRIPTION: "D" for discrete feature,"L" for left split of
-        contimuous feature,"R"for left split of contimuous feature
-        Returns
-        -------
-        splitedData: data splits
-        """
-        splitedData = []  # store splited data
-        if feature_type == "D":  # for discrete feature
-            for feat_vect in train_data:
-                if feat_vect[feature_index] == feature_value:
-                    reducedVect = []
-                    # delete used discrete feature from data
-                    for i in range(len(feat_vect)):
-                        if i < feature_index or i > feature_index:
-                            reducedVect.append(feat_vect[i])
-                    splitedData.append(reducedVect)
-        if feature_type == "L":  # for continous feature
-            for feat_vect in train_data:
-                if feat_vect[feature_index] <= feature_value:
-                    splitedData.append(feat_vect)
-        if feature_type == "R":  # for continous feature
-            for feat_vect in train_data:
-                if feat_vect[feature_index] > feature_value:
-                    splitedData.append(feat_vect)
-        return splitedData
+        shanonEnt=0
+        for key in lable_counts:
+            prob = float(lable_counts[key])/sum
+            shanonEnt -= prob * log(prob, 2)
 
-    # choose best feature to split
-    def choose_split_feature(self, train_data):
-        """
-        Parameters
-        ----------
-        train_data : list
-        DESCRIPTION: list of training instances
-        Returns
-        -------
-        best_feat_index: index of chosen feature
-        best_feat_value: value of chosen feature
-        """
-        feat_num = len(train_data[0]) - 1  # get available features
-        base_ent = self.Entropy(train_data)
-        bestInforGain = 0.0
-        best_feat_index = -1
-        best_feat_value = 0
-        for i in range(feat_num):
-            if isinstance(train_data[0][i], str):  # for discrete feature
-                feat_list = [example[i] for example in train_data]
-                unique_values = set(feat_list)
-                newEnt = 0
-                for value in unique_values:
-                    sub_data = self.split_data(train_data, i, value, "D")
-                    prop = float(len(sub_data)) / len(train_data)
-                    newEnt += prop * self.Entropy(sub_data)  # see Eq.(3.2)
-                inforgain = base_ent - newEnt
-                if inforgain > bestInforGain:
-                    best_feat_index = i
-                    bestInforGain = inforgain
-            else:  # for continous feature
-                feat_list = [example[i] for example in train_data]
-                unique_values = set(feat_list)
-                sort_unique_values = sorted(unique_values)
-                minEnt = np.inf
-                for j in range(len(sort_unique_values) - 1):
-                    div_value = (sort_unique_values[j] + sort_unique_values[j + 1]) / 2
-                    sub_data_left = self.split_data(train_data, i, div_value, "L")
-                    sub_data_right = self.split_data(train_data, i, div_value, "R")
-                    prop_left = float(len(sub_data_left)) / len(train_data)
-                    prop_right = float(len(sub_data_right)) / len(train_data)
-                    ent = prop_left * self.Entropy(sub_data_left) + \
-                          prop_right * self.Entropy(sub_data_right)  # see Eq.(3.6)
-                    if ent < minEnt:
-                        minEnt = ent
-                        best_feat_value = div_value
-                inforgain = base_ent - minEnt
-                if inforgain > bestInforGain:
-                    bestInforGain = inforgain
-                    best_feat_index = i
-        return best_feat_index, best_feat_value
+        return shanonEnt
 
-    # get major class
-    def get_major_class(self, classList):
-        """
-        Parameters
-        ----------
-        classList : TYPE: list
-        DESCRIPTION: labels of instances
-        Returns
-        -------
-        major: TYPE: int
-        DESCRIPTION: major label
-        """
-        classcount = {}
-        for vote in classList:
-            if vote not in classcount.keys():
-                classcount[vote] = 0
-            classcount[vote] += 1
-        sortedclasscount = sorted(classcount.items(), key=operator.itemgetter(1), reverse=True)
-        major = sortedclasscount[0][0]
-        return major
 
-    # create decision tree
-    def create_decision_tree(self, train_data, feat_names, tmp=0):
-        classList = [example[-1] for example in train_data]
-        # classList = []
-        # for x in tmpclassList:
-        #     classList.append([x])
-        if len(train_data) == 0:  # see condition C
-            return
-        if len(train_data[0]) == 1:  # see condition B
-            return self.get_major_class(classList)
-        print(classList.count(classList[0]))
-        print(len(classList))
-        if classList.count(classList[0]) == len(classList) or classList.count(classList[0])==self.tmp:  # see condition A
-            return classList[0]
-        self.tmp=classList.count(classList[0])
-        # choose best division feature
-        best_feat, best_div_value = self.choose_split_feature(train_data)
-        if isinstance(train_data[0][best_feat], str):  # for discrete feature
-            feat_name = feat_names[best_feat]
-            tree_model = {feat_name: {}}  # generate a root node
-            del (feat_names[best_feat])  # del feature used
-            feat_values = [example[best_feat] for example in train_data]
-            unique_feat_values = set(feat_values)
-            # create a node for each value of the best feature
-            for value in unique_feat_values:
-                sub_feat_names = feat_names[:]
-                tree_model[feat_name][value] = \
-                    self.create_decision_tree(self.split_data(train_data,
-                                                              best_feat, value, "D"),
-                                              sub_feat_names)
-        else:  # for contiunous feature
-            print(train_data[0][best_feat])
-            best_feat_name = feat_names[best_feat] + "<" + str(best_div_value)
-            tree_model = {best_feat_name: {}}  # generate a root node
-            sub_feat_names = feat_names
-            # generate left node
-            tree_model[best_feat_name]["Y"] = \
-                self.create_decision_tree(self.split_data(train_data,
-                                                          best_feat,
-                                                          best_div_value, "L"),
-                                          sub_feat_names)
-            # generate right node
-            tree_model[best_feat_name]["N"] = \
-                self.create_decision_tree(self.split_data(train_data,
-                                                          best_feat,
-                                                          best_div_value, "R"),
-                                          sub_feat_names)
-        return tree_model
-
-    # define predict function
-    def predict(self, tree_model, feat_names, feat_vect):
-        """
-        Parameters
-        ----------
-        tree_model : dict
-        DESCRIPTION: decision tree model
-        feat_names : list
-        DESCRIPTION: feature names
-        feat_vect :
-        DESCRIPTION: feature vector
-        Returns
-        -------
-        label: predicted label
-        """
-        firstStr = list(tree_model.keys())[0]  # get tree root
-        lessIndex = str(firstStr).find('<')
-        if lessIndex > -1:  # if root is a continous feature
-            # recursively search untill leaft node
-            secondDict = tree_model[firstStr]
-            feat_name = str(firstStr)[:lessIndex]
-            featIndex = feat_names.index(feat_name)
-            div_value = float(str(firstStr)[lessIndex + 1:])
-            if np.any(feat_vect[featIndex] <= div_value):
-                if isinstance(secondDict["Y"], dict):
-                    classLabel = self.predict(secondDict["Y"],
-                                              feat_names, feat_vect)
-                else:
-                    classLabel = secondDict["Y"]
+    # feature_index表示使用哪个特征分割数据集，value表示特征的取值
+    def split_dataset(self, dataset, feature_index, value):
+        sub_dataset_l = []
+        sub_dataset_r = []
+        for sample in dataset:
+            if sample[feature_index] > value:
+                sub_dataset_r.append(sample.copy())
             else:
-                if isinstance(secondDict["N"], dict):
-                    classLabel = self.predict(secondDict["N"],
-                                              feat_names, feat_vect)
-                else:
-                    classLabel = secondDict["N"]
-            return classLabel
-        else:  # if root is a discrete feature
-            # recursively search untill leaft node
-            secondDict = tree_model[firstStr]
-            featIndex = feat_names.index(firstStr)
-            key = feat_vect[featIndex]
-            valueOfFeat = secondDict[key]
-            if isinstance(valueOfFeat, dict):
-                classLabel = self.predict(valueOfFeat, feat_names, feat_vect)
-            else:
-                classLabel = valueOfFeat
-            return classLabel
+                sub_dataset_l.append(sample.copy())
+        return sub_dataset_l, sub_dataset_r
 
 
-DC = Decision_Tree_C45()
+    def choose_best_feature(self, dataset, weight):
+        feature_num = len(dataset[0])-1
+        dataset_Ent = self.cal_Ent(dataset, weight)
+        best_feature = 0
+        best_value = 0
+        best_info_gain = 0.0
+        for i in range(feature_num):  # 按特征的排列顺序，计算各个特征的信息增益
+            values = [example[i] for example in dataset]  # 第i个特征的所有取值
+            unique_values = list(set(values))  # 无序不重复集合
+            unique_values.sort()  # 按取值的从小到大排序
+            t_a = [(unique_values[i+1]+unique_values[i])/2 for i in range(len(unique_values)-1)]   # 相邻两数的中间值
+
+            for value in t_a:
+                sub_dataset_l, sub_dataset_r = self.split_dataset(dataset, i, value)  # 提取出第i个特征等于value的子集
+                sub_Ent = len(sub_dataset_l)/float(len(dataset))*self.cal_Ent(sub_dataset_l, weight)\
+                          +len(sub_dataset_r)/float(len(dataset))*self.cal_Ent(sub_dataset_r, weight)# 每个子集的经验熵加权求和， 即经验条件熵
+
+                info_gain = dataset_Ent - sub_Ent  # 计算该特征的信息增益
+                if info_gain > best_info_gain:   # 取信息增益最大的特征作为最优特征
+                    best_info_gain = info_gain
+                    best_feature = i
+                    best_value = value
+        return best_feature, best_value
+
+
+    # 计算子集中数量最多的类别，并返回类别键值
+    def majority_count(self, classlist, weight):
+        classes = {}
+        for i in range(len(classlist)):
+            if classlist[i] not in list(classes.keys()):
+                classes[classlist[i]] = 0
+            classes[classlist[i]] += weight[i][0]
+
+        sorted_classes = sorted(classes.items(), key=operator.itemgetter(1), reverse=True)  # 以索引为1的值来进行排序
+        return sorted_classes[0][0]
+
+
+    def fit(self, dataset, weight):
+
+        best_feature, best_value = self.choose_best_feature(dataset, weight)
+        self.stump[0] = best_feature
+        self.stump[1] = best_value
+
+        sub_dataset_l, sub_dataset_r = self.split_dataset(dataset, best_feature, best_value)
+
+        classlist_l = [sample[-1] for sample in sub_dataset_l]
+        self.stump[2] = self.majority_count(classlist_l, weight)
+
+        classlist_r = [sample[-1] for sample in sub_dataset_r]
+        self.stump[3] = self.majority_count(classlist_r, weight)
+
+    def predict(self, x):
+        feature_index = x[:, self.stump[0]].reshape([-1,1])
+        pred = np.where(feature_index < self.stump[1], self.stump[2], self.stump[3])
+
+        return pred
+
+
+
+
+
 train_x = np.array(X_train).reshape(-1, 5)
 train_y = np.array(Y_train).reshape(-1, 1)
-train = numpy.concatenate((train_x, train_y), axis=1)
+train_dataset = numpy.concatenate((train_x, train_y), axis=1)
 test_x = np.array(X_test).reshape(-1, 5)
 test_y = np.array(Y_test).reshape(-1, 1)
-featnames = ['收入', '年龄', '性别', '历史授信额度', '历史违约次数', '是否违约']
-treemodel = DC.create_decision_tree(train, featnames)
-# 进行预测
-predicted_y = DC.predict(treemodel, featnames, test_x)
-# 评估模型的预测性能
+featnames = ['收入', '年龄', '性别', '历史授信额度', '历史违约次数']
+weight=np.ones([1000,1])
+
+DC = Decision_Tree_C45()
+DC.fit(train_dataset, weight)
+predicted_y = DC.predict(test_x)
 mse = mean_squared_error(test_y, predicted_y)
 r2 = r2_score(test_y, predicted_y)
 
